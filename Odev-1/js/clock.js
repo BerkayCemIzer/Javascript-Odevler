0056e6d497d00607c37e0684924594a82086abf7
Berkay Cem İzer <94552755+BerkayCemIzer@users.noreply.github.com>
2022-02-12 13:32:19 +0300

    Added clock.js

---
+++ Odev-1/js/clock.js
@@ -0,0 +1,22 @@
+function gonder() {
+    let a = document.querySelector("#yazi").value;
+    document.querySelector("#myName").innerHTML = a;
+    document.querySelector("#alan").style.display = "block";
+    document.querySelector("#isim").style.display = "none";
+}
+
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
